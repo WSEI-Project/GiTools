bbeb84461570d61ef6cd18f77fe75857131eb07e
Grzegorz Ociepka <grzegorz.ociepka@oroox.com>
2019-11-05 15:03:33 +0100

    update CreateCommit method

--- GiTools/Services/GitService.cs
+++ GiTools/Services/GitService.cs
@@ -1,4 +1,5 @@
 ﻿using Octokit;
+using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 
@@ -11,28 +12,31 @@ namespace GiTools.Services
             .GetCustomAttribute<AssemblyProductAttribute>()
             .Product;
         private GitHubClient github;
-        public async Task CreateCommit(string owner,string repo)
+        public async Task CreateCommit(string owner, string repo, string directoryToAdd)
         {
             var headMasterRef = "heads/master";
             github = GetClient(new ProductHeaderValue(GitHubIdentity), "token");
             var masterReference = await github.Git.Reference.Get(owner, repo, headMasterRef);
             var latestCommit = await GetLatestSHA(owner, repo, headMasterRef);
-            var textBlobRef = await CreateTextBlob(owner, repo, "/path/to/file");
 
             var nt = new NewTree { BaseTree = latestCommit.Tree.Sha };
-            var newTreeItem = new NewTreeItem { Mode = "100644", Type = TreeType.Blob, Content = "file.content", Path = "path/to/file.txt" };
-            nt.Tree.Add(newTreeItem);
+            string[] filePaths = Directory.GetFiles(string.Format(@"{0}\", directoryToAdd));
+
+            foreach (var filePath in filePaths)
+            {
+                var linesOfCode =  await File.ReadAllLinesAsync(filePath);
+                
+                var newTreeItem = new NewTreeItem { Mode = "100644", Type = TreeType.Blob, Content = linesOfCode.ToString() , Path = filePath };
+                nt.Tree.Add(newTreeItem);
+            }
 
             var newTree = await github.Git.Tree.Create(owner, repo, nt);
             var newCommit = new NewCommit("Commit test with several files", newTree.Sha, masterReference.Object.Sha);
             var commit = await github.Git.Commit.Create(owner, repo, newCommit);
             await github.Git.Reference.Update(owner, repo, headMasterRef, new ReferenceUpdate(commit.Sha));
+
         }
-        private async Task<BlobReference> CreateTextBlob(string owner, string repo, string pathToFile)
-        {
-            var textBlob = new NewBlob { Encoding = EncodingType.Utf8, Content = "Hellow World!" };
-            return await github.Git.Blob.Create(owner, repo, textBlob);
-        }
+
         private async Task<Commit> GetLatestSHA(string owner, string repo, string headMasterRef)
         {
             var masterReference = await github.Git.Reference.Get(owner, repo, headMasterRef);
