78bf7a0ee0f746e9f6f8bec42a1d9359b8e7d4c2
Grzesiek <grzegorzociepka09@gmail.com>
2020-01-03 21:41:49 +0100

    Dodaj pliki projektów.

---
+++ Services/Interfaces/IGitService.cs
@@ -0,0 +1,19 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiTools.Services.Interfaces
+{
+    interface IGitService
+    {
+        Task CreateCommit(string owner, string repo, string directoryToAdd, string commitText);
+        Task CreateRepo(string repoName, bool isPrivate);
+        Task<IReadOnlyList<RepositoryContributor>> GetContributors(long repoId);
+        Task<SearchRepositoryResult> SearchRepositories(SearchRepositoriesRequest req);
+        Task<CommitActivity> GetCommitActivity(long repoId);
+        Task DownloadRepo(string owner, string name, string path);
+        Task<CodeFrequency> GetCodeFrequency(long repoId);
+    }
+}
