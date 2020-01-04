ff0c78b5422ba91c90384f6a71957ea76504bcc8
Mateusz Hanzel <matek997@outlook.com>
2020-01-04 14:10:10 +0100

    Setup tests project
    Structure, files and demo token class

---
+++ GiToolsTests/Services/GitServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace GiToolsTests.Services
+{
+    public class GitServiceTests
+    {
+        /*
+        public GitService(string token)
+        public async Task CreateCommit(string owner, string repo, string directoryToAdd, string commitText)
+        public async Task CreateRepo(string repoName, bool isPrivate)
+        public async Task<IReadOnlyList<RepositoryContributor>> GetContributors(long repoId)
+        public async Task<SearchRepositoryResult> SearchRepositories(SearchRepositoriesRequest req)
+        public async Task<CommitActivity> GetCommitActivity(long repoId)
+        public async Task<CodeFrequency> GetCodeFrequency(long repoId)
+        public async Task DownloadRepo(string owner, string name, string path)
+        */   
+    }
+}
---
+++ GiToolsTests/Token_example.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiToolsTests
+{
+    /**
+     * rename this file to Token.cs
+     * Enter test tokens here
+     */
+    class GitTestToken
+    {
+        public static readonly string MinimalPermissions = "TOKEN";
+        public static readonly string FullPermissions = "TOKEN";
+    }
+}
