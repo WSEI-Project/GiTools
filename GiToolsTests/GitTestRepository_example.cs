0335c4c63b24b2f566ab9039350e1331bf8fa7b8
Mateusz Hanzel <matek997@outlook.com>
2020-01-04 15:04:11 +0100

    Add demo repositories
    Class holding repositories used in tests

---
+++ GiToolsTests/GitTestRepository_example.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiToolsTests
+{
+    /**
+     * rename this file to GitTestRepository.cs
+     * test repositories go here
+     */
+    class GitTestRepository
+    {
+        /**
+         * public repository
+         */
+        public static readonly long Public = 0;
+        /**
+         * private repository (should be only accessible with correct permissions set)
+         */
+        public static readonly long Private = 0;
+    }
+}
