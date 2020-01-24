6615c8f98d30ac192baed78463ef3e9f6f1c3235
Przzemek <he1634@interia.pl>
2020-01-24 01:27:04 +0100

    Updated MainWindow

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -3,6 +3,11 @@ using GiTools;
 using GiTools.Services;
 using System.Windows;
 using System.Windows.Controls;
+using System.Net.Http;
+using System;
+using System.Windows.Input;
+using System.Diagnostics;
+using System.ComponentModel;
 
 namespace GiTools
 {
@@ -11,23 +16,50 @@ namespace GiTools
     /// </summary>
     public partial class MainWindow : Window
     {
-       
+
 
         public MainWindow()
         {
             InitializeComponent();
+            CloseButton.Click += (s, e) => Close();
+        }
+        public void Token_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.YouTube.com");
         }
 
+        #region Window: Moving
+
+        private void GridOfWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var move = sender as System.Windows.Controls.Grid;
+            var win = Window.GetWindow(move);
+            win.DragMove();
+        }
+
+        #endregion
+
+        #region Window : Button1
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(TokenInputBox.Text))
                 MessageBox.Show("No token");
             else
             {
-                Home home = new Home(TokenInputBox.Text);
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://api.github.com");
+                var token = TokenInputBox.Text;
+                client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+
+                Home home = new Home(token);
                 home.Show();
                 this.Close();
             }
         }
+        #endregion
+
+
     }
 }
