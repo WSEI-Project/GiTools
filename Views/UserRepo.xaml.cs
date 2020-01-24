c47717da24d916a4e4ea8b8b4a0fd1fd73dcd421
Mateusz Hanzel <matek997@outlook.com>
2020-01-24 20:25:52 +0100

    Remove unnecessary directives

--- Views/UserRepo.xaml.cs
+++ Views/UserRepo.xaml.cs
@@ -1,18 +1,10 @@
 ﻿using GiTools.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Octokit;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace GiTools.Views
 {
