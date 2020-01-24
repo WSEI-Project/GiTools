WSEI-Project/GiTools

commit 10dcdab6f80576b64bcef50472c4714602ecc87a

Fix for crash from saving errors
Fixes crashes created by lack of permissions and directories not existing.

Views/UserRepo.xaml.cs
@@ -24,7 +24,7 @@ namespace GiTools.Views
         GitService git;
         List<Repo> dataTableData;
 
-
+        private readonly string savingPath = "C:\\nowrite\\Projects";
         public UserRepo(GitService git)
         {
             InitializeComponent();
@@ -63,9 +63,21 @@ namespace GiTools.Views
         private async void DownloadRepo_Click(object sender, RoutedEventArgs e)
         {
             var myValue = ((Button)sender).Tag.ToString();
-
-            await git.DownloadRepo(long.Parse(myValue), "C:\\Projekty");
-            MessageBox.Show("Repository has been downloaded");
+            if(!System.IO.Directory.Exists(savingPath))
+            {
+                try
+                {
+                    System.IO.Directory.CreateDirectory(savingPath);
+                    await git.DownloadRepo(long.Parse(myValue), savingPath);
+                    MessageBox.Show($"Repository has been downloaded to {savingPath}");
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show($"Repository could not be saved due to error: {ex.Message}");
+                }
+                
+            }
+  
         }
         #endregion
 

commit 659dedd7a3a3dc706f9f077cdc58c750946817af

Fix bug introduced in 10dcdab6f80576b64bcef50472c4714602ecc87a

Views/UserRepo.xaml.cs
@@ -62,8 +62,7 @@ namespace GiTools.Views
                 try
                 {
                     System.IO.Directory.CreateDirectory(savingPath);
-                    await git.DownloadRepo(long.Parse(myValue), savingPath);
-                    MessageBox.Show($"Repository has been downloaded to {savingPath}");
+                    
                 }
                 catch(Exception ex)
                 {
@@ -71,7 +70,16 @@ namespace GiTools.Views
                 }
                 
             }
-  
+            try
+            {
+                await git.DownloadRepo(long.Parse(myValue), savingPath);
+                MessageBox.Show($"Repository has been downloaded to {savingPath}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Repository could not be saved due to error: {ex.Message}");
+            }
+
         }
        
         private async void ShowContributors_Click(object sender, RoutedEventArgs e)